684222420e0ffb83b2e50bf10857eaddeed25f84
Yasmeen Yousef <yasmeen.yousef05@gmail.com>
2024-11-03 18:21:51 -0500

    update

--- server/models.py
+++ server/models.py
@@ -1,101 +1,131 @@
-from sqlalchemy.orm import validates
-from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy import Column, Integer, String, Text, DateTime, ForeignKey
+from sqlalchemy.orm import validates, relationship
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from marshmallow import Schema, fields, post_load
 from config import db, bcrypt
-from flask import abort
-from sqlalchemy_serializer import SerializerMixin
 
-# Models go here:
-class User( db.Model, SerializerMixin ):
+# Models:
+
+class User( db.Model):
     __tablename__ = 'users'
-    serialize_rules = ('-books_in_lists.user',)
 
-    id = db.Column( db.Integer, primary_key=True )
-    username = db.Column( db.String )
-    email = db.Column( db.String, unique = True )
-    password = db.Column( db.Text )
-    image = db.Column( db.String, default="https://vdostavka.ru/wp-content/uploads/2019/05/no-avatar.png" )
-    books_in_lists = db.relationship( "List", backref = "user" )
+    id = Column(Integer, primary_key=True)
+    username = Column(String, nullable=False)
+    email = Column(String, unique=True, nullable=False)
+    password = Column(Text, nullable=False)
+    image = db.Column(String, default="https://vdostavka.ru/wp-content/uploads/2019/05/no-avatar.png" )
+    created_at = Column(DateTime, default=db.func.current_timestamp())
+    updated_at = Column(DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
 
-    def authenticate( self, password ):
-        return bcrypt.check_password_hash(
-            self.password, password.encode( 'utf-8' )
-        )
+    lists = db.relationship("List", backref="user")
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(self.password, password)
+    
+    @validates('password')
+    def validate_password(self, key, password):
+        # Example validation for password strength
+        if len(password) < 8:
+            raise ValueError("Password must be at least 8 characters long.")
+        return password
 
 
-class Book( db.Model, SerializerMixin ):
+class Book( db.Model):
     __tablename__ = 'books'
-    serialize_rules = ('-book_authors.book','-links.book',)
 
-    id = db.Column( db.Integer, primary_key=True )
-    title = db.Column( db.String )
-    key = db.Column( db.String, unique = True  )
-    description = db.Column( db.String, default=None )
-    publisher = db.Column( db.String, default=None )
-    language = db.Column( db.String, default=None )
-    isbn = db.Column( db.String, default=None )
-    publish_date  = db.Column( db.String, default=None )
-    rating = db.Column( db.Integer, default=None )
-    rating_count = db.Column( db.Integer, default=None )
-    author = db.Column( db.String, default=None )
-    cover = db.Column( db.String, default="https://islandpress.org/sites/default/files/default_book_cover_2015.jpg" )
-    subjects = db.Column( db.String, default=None )
+    id = Column(Integer, primary_key=True)
+    title = Column(String, nullable=False)
+    key = Column(String, unique=True, nullable=False)
+    description = Column(Text, default=None)
+    publisher = Column(String, default=None)
+    language = Column(String, default=None)
+    isbn = Column(String, default=None)
+    publish_date = Column(String, default=None)  # Consider changing to Date
+    rating = Column(Integer, default=None)
+    rating_count = Column(Integer, default=None)
+    cover = Column(String, default="https://islandpress.org/sites/default/files/default_book_cover_2015.jpg")
+    subjects = Column(String, default=None)
 
-    book_authors = db.relationship( "BookAuthor", backref = "book")
-    links = db.relationship( "BookLink", backref = "book" )
+    book_in_lists = relationship("BookInList", backref="book")
 
-
-class Author( db.Model, SerializerMixin ):
-    __tablename__ = 'authors'
-    serialize_rules = ('-books.author',)
-
-    id = db.Column( db.Integer, primary_key=True )
-    name = db.Column( db.String )
-    birth_date  = db.Column( db.String )
-    bio = db.Column( db.String )
-    work_count = db.Column( db.Integer )
-
-    books = db.relationship( "BookAuthor", backref = "author")
-
-class List( db.Model,SerializerMixin ):
+class List(db.Model):
     __tablename__ = 'lists'
-    serialize_rules = ('-books.lists',)
+    
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    created_at = Column(DateTime, default=db.func.current_timestamp())
+    updated_at = Column(DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
 
-    id = db.Column( db.Integer, primary_key=True )
-    name = db.Column( db.String )
-    user_id = db.Column( db.Integer, db.ForeignKey( 'users.id' ) )
-    books = db.relationship( "BookInList", backref = "list")
+    books = relationship("BookInList", backref="list")
 
-class BookInList( db.Model, SerializerMixin ):
+class BookInList(db.Model):
     __tablename__ = 'books_in_lists'
-    serialize_rules = ('-book.books_in_lists','-user.books_in_lists','-list.books_in_lists')
+    
+    id = Column(Integer, primary_key=True)
+    book_id = Column(Integer, ForeignKey('books.id'), nullable=False)
+    list_id = Column(Integer, ForeignKey('lists.id'), nullable=False)
 
-    id = db.Column( db.Integer, primary_key=True )
-    book_name = db.Column( db.String )
-    book_cover = db.Column( db.String )
-    book_id = db.Column( db.Integer, db.ForeignKey( 'books.id' ) )
-    list_id = db.Column( db.Integer, db.ForeignKey( 'lists.id' ) )
-    user_id = db.Column( db.Integer, db.ForeignKey( 'users.id' ) )
+    @validates('book_id')
+    def validate_book_id(self, key, book_id):
+        if not isinstance(book_id, int) or book_id <= 0:
+            raise ValueError("Book ID must be a positive integer.")
+        return book_id
 
-class BookAuthor( db.Model, SerializerMixin ):
-    __tablename__ = 'book_authors'
-    serialize_rules = ('-book.BookAuthor','-author.BookAuthor')
+    @validates('list_id')
+    def validate_list_id(self, key, list_id):
+        if not isinstance(list_id, int) or list_id <= 0:
+            raise ValueError("List ID must be a positive integer.")
+        return list_id
+    
 
-    id = db.Column( db.Integer, primary_key=True )
-    book_id = db.Column( db.Integer, db.ForeignKey( 'books.id' ) )
-    author_id = db.Column( db.Integer, db.ForeignKey( 'authors.id' ) )
+# Serialization Schemas
 
-class BookLink(db.Model, SerializerMixin ):
-    __tablename__ = 'links'
-    serialize_rules = ('-book.BookLink',)
+class UserSchema(Schema):
+    id = fields.Int()
+    username = fields.Str()
+    email = fields.Str()
+    image = fields.Str()
+    lists = fields.List(fields.Nested('ListSchema', exclude=('user',)))
 
-    id = db.Column( db.Integer, primary_key=True )
-    book_key = db.Column( db.Integer, db.ForeignKey( 'books.key' ) )
-    name = db.Column( db.String )
-    url = db.Column( db.String, unique = True )
+    @post_load
+    def make_user(self, data, **kwargs):
+        return User(**data)
 
-    @validates( 'user_id' )
-    def validate_user_id( self, key, user_id ):
-        if isinstance( user_id, int ) and user_id > 0:
-            return user_id
-        else:
-            abort( 422, "User id must be a number greater than 0." )
+class BookSchema(Schema):
+    id = fields.Int()
+    title = fields.Str()
+    key = fields.Str()
+    description = fields.Str()
+    publisher = fields.Str()
+    language = fields.Str()
+    isbn = fields.Str()
+    publish_date = fields.Str()
+    rating = fields.Int()
+    rating_count = fields.Int()
+    cover = fields.Str()
+    subjects = fields.Str()
+
+    @post_load
+    def make_book(self, data, **kwargs):
+        return Book(**data)
+
+class ListSchema(Schema):
+    id = fields.Int()
+    name = fields.Str()
+    user_id = fields.Int()
+    books = fields.List(fields.Nested('BookInListSchema'))
+
+    @post_load
+    def make_list(self, data, **kwargs):
+        return List(**data)
+
+class BookInListSchema(Schema):
+    id = fields.Int()
+    book_id = fields.Int()
+    list_id = fields.Int()
+
+    @post_load
+    def make_book_in_list(self, data, **kwargs):
+        return BookInList(**data)
